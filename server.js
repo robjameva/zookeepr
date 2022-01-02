38beb59af14a584ce2bdcb0ad882cda8943802d8
Robert Evanik <rob.evanik@gmail.com>
2022-01-02 10:40:29 -0500

    Linked front end and back end

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
@@ -80,6 +82,21 @@ app.post('/api/animals', (req, res) => {
     }
 });
 
+app.get('/', (res, req) => {
+    req.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (res, req) => {
+    req.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (res, req) => {
+    req.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (res, req) => {
+    req.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 app.listen(PORT, () => console.log(`API server is now on port ${PORT}`))
